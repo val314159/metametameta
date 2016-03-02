ace233ff86c907d2e3c41563065fc76585e5a4a6
Joel Ward <jmward@gmail.com>
2016-03-01 21:58:04 -0800

    add ls-lart

--- static/main.js
+++ static/main.js
@@ -1,32 +1,45 @@
 function appendEditor(elt){
-    var lbl0=gensym(),lbl1=gensym(),lbl2=gensym();
-    var lblD=gensym(),lblE=gensym(),lblP=gensym();
-    elt.appendChild(EDIT(ID(lbl0,H3("/static/FILENAME"))));
-    elt.appendChild(ID(lblD,codeDiv('b1 db',lblE)));
-    EDIT(document.getElementById(lblE));
-    elt.appendChild(newElt('hr'));
-    elt.appendChild(ID(lbl1,newElt('button','save')));
-    elt.appendChild(ID(lbl2,newElt('button','load')));
-    elt.appendChild(ID(lblP,newElt('button','preview')));
-    function filename(){return $('#'+lbl0).html();}
-    function filedata(){return $('#'+lblE).html();}
+    var lbl0=gensym(),lbl1=gensym(),lbl2=gensym()
+    var lblD=gensym(),lblE=gensym(),lblP=gensym()
+    elt.appendChild(EDIT(ID(lbl0,H3("/static/FILENAME"))))
+    elt.appendChild(ID(lblD,codeDiv('b1 db',lblE)))
+    EDIT(document.getElementById(lblE))
+    elt.appendChild(newElt('hr'))
+    elt.appendChild(ID(lbl1,newElt('button','save')))
+    elt.appendChild(ID(lbl2,newElt('button','load')))
+    elt.appendChild(ID(lblP,newElt('button','preview')))
+    function filename(){return $('#'+lbl0).html()}
+    function filedata(){return $('#'+lblE).html()}
     function url(){
-	var url = filename();
-	return ( url[0]=='/' ? '_' : '_/' ) + url;}
+	var url = filename()
+	return ( url[0]=='/' ? '_' : '_/' ) + url}
     $('#'+lbl1).click(function(ev){
 	$.post(url(),filedata(),{
 	}).then(function(a){
-	    console.log("saved "+url);
+	    console.log("saved "+url)
 	}).then(null,function(a,b,c,d){
-	    console.log("F",[a,b,c,d]);
-	});    });
+	    console.log("F",[a,b,c,d])
+	})    })
     $('#'+lbl2).click(function(ev){
 	$('#'+lblE).load(url(),function(data,err,xhr){
-	    console.log("FINISHED",[data,err,xhr]);
-	});    });
+	    console.log("FINISHED",[data,err,xhr])
+	})    })
     $('#'+lblP).click(function(ev){
-	var arr = filename().split('/');
-	if (arr[0]=='')  arr = arr.splice(1);
-	window.open([''].concat(arr.splice(1)).join('/'));
-    }); }
-$('#createBuffer').click(appendEditor.bind(null,document.body));
+	var arr = filename().split('/')
+	if (arr[0]=='')  arr = arr.splice(1)
+	window.open([''].concat(arr.splice(1)).join('/'))
+    })}
+function refreshDir(elt_name){
+    var elt=$(elt_name)
+    $.get('/_').then(function(data){
+	elt.html('')
+	var pfx = data.result.prefix
+	var files=data.result.files
+	for (var n=0; n<files.length; n++){
+	    for (var m=0; m<files[n].length; m++){
+		var f = pfx+files[n][m]
+		var html = "<a href='"+f+"'>"+f+"</a>"
+		elt.append(newElt('li',html))
+	    }}})}
+$('#createBuffer').click(appendEditor.bind(null,document.body))
+$('#refreshDir').click(refreshDir.bind(null,"#dir-viewport"))
--- ws.py
+++ ws.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env bottle.py -b:7000 -sgevent --debug --reload ws --
-import pyjade,markdown,coffeescript
+import pyjade,markdown,coffeescript,os,sys,json
 from bottle import request,response,static_file,route,get,put,post,error
 def ropen(name,ext): return open('tmpl/%s.%s'%(name,ext)).read()
 def wopen(pfx,name,txt): return open(pfx+'/'+name,'w').write(txt)
 @route('/_/<prefix>/<name:path>')
-def _(prefix,name): return static_file(name,root=prefix)
+def _(prefix,name): return static_file(name,root=prefix,mimetype='text/plain')
 @post('/_/<prefix>/<name:path>')
 def _(prefix,name):wopen(prefix,name,request.body.read());return['ok']
+@route('/_')
+def _(): return dict(result=dict(prefix='/_/',
+                                 files=getfiles('static')+getfiles('tmpl')))
+def getfiles(pfx): return [ [ prefix+'/'+f for f in files ]
+                            for prefix, dirs, files in os.walk(pfx) ]
 @route('/')
 @route('/<name:path>')
 def _(name='index'):
@@ -21,5 +26,7 @@ def _(name='index'):
     try:   return [ ropen(name,'js') ]
     except IOError: pass
     return static_file(name, root='./static/')
+@route('/_/*')
+def _(): return static_file('',root='./static')
 @error(404)
 def _(error): return ['Not Found\n']
